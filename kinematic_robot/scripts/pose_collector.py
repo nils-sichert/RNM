dba200f3347ca87630a98113a90dbeda9a65507f
MTEC Lab right PC <cem@guelsan.com>
2022-07-04 18:12:38 +0200

    recorded new data mr cc

--- kinematic_robot/scripts/pose_collector.py
+++ kinematic_robot/scripts/pose_collector.py
@@ -38,11 +38,11 @@ class pose_collector():
         # rostopic pub /position_reached std_msgs/Bool True
 
 
-        self.MR_path_dataset = os.path.join(os.path.dirname(__file__), 'CV_model_registration_data/dataset004')
+        self.MR_path_dataset = os.path.join(os.path.dirname(__file__), 'CV_model_registration_data/dataset009')
         self.CC_path_dataset = os.path.join(os.path.dirname(__file__), 'CV_camera_calibration_data/dataset001')
 
         # Set this path to self.CC_path_dataset or self_MR_path_dataset
-        self.path_dataset = self.CC_path_dataset
+        self.path_dataset = self.MR_path_dataset
 
 
         self.node_name = "pose_collector"
