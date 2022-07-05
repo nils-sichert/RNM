30dbda8d80730c6cfe0aaafd249509905e46d2a1
Danny Jordan <danny.jordan@tuhh.de>
2022-07-05 06:55:39 +0200

    debugged  hand in eye working now

--- kinematic_robot/scripts/CV_hand_in_eye.py
+++ kinematic_robot/scripts/CV_hand_in_eye.py
@@ -18,10 +18,17 @@ from sympy import true
 class CameraCalibration():
 
     def __init__(self):
+        # Initialize ROS Specific Functions
+        self.node_name = "hand_in_eye"
+        rospy.init_node(self.node_name)
+
         # Start and Finish Flags
-        self.task_start_flag = False
-        self.task_start_command = "handeye_calibration_start"
-        self.task_finished_command = "handeye_calibration__finished"
+        self.start_task = False
+        self.TASKCMD = "handeye_calibration_start"
+        self.TASKFIN = "handeye_calibration__finished"
+
+        #HomogenousMatrix Result
+        self.cam2gripper_hm = []
 
         # Load Camera Calibration Results
         self.result_path = os.path.join(os.path.dirname(__file__),'CV_camera_calibration_results')  
@@ -40,27 +47,48 @@ class CameraCalibration():
 
         # Wait until process_manager is ready
         rospy.logwarn('[HiE] Waiting for PM...')
-        while not self.task_start_flag: pass
+        self.wait_for_task_command()
         rospy.logwarn('[HiE] Received task_start_command')
-        time.sleep(2)
         
-        self.R_cam2gripper, self.t_cam2gripper = 	cv.calibrateHandEye(self.R_gripper2base, self.t_gripper2base, self.rmat_rgb, self.tvecs_rgb, method = cv.CALIB_HAND_EYE_PARK)
+        
+        self.R_cam2gripper, self.t_cam2gripper = cv.calibrateHandEye(self.R_gripper2base, self.t_gripper2base, self.rmat_rgb, self.tvecs_rgb, method = cv.CALIB_HAND_EYE_PARK)
         
         # Saves result of R- t and HM in CV_camera_calibration folder
         self.save_result() 
 
 
         # Give Feedback to PM
-        self.pub_task_finished.publish(self.task_finished_command)
+        self.pub_task_finished.publish(self.TASKFIN)
         rospy.logwarn('[HiE] Main process finished')
 
+    def rmat_tvec_to_hm(self, rmat, tvec):
+        """return pose_hm"""
+        temp = [0, 0, 0 ,1]
 
+        pose_3x4 = np.c_[rmat, np.reshape(tvec, (3,1))]
+        pose_hm = np.r_[pose_3x4, np.reshape(temp, (1,4))]
+        return pose_hm
+
+    # Task control
     def callback_task_command(self, msg : String):
-        ''' /TODO Docstring'''
-        if msg.data == self.task_start_command:
-            self.task_start_flag = True
-        else:
-            self.task_start_flag = False
+        ''' Listenes for task command and sets self.start_task '''
+
+        msg = msg.data
+        if msg == self.TASKCMD:
+            rospy.logwarn(f'[CC] Received correct start command "{msg}"')
+            self.start_task = True
+
+
+    def publish_task_finished(self):
+        msg = String()
+        msg.data = self.TASKFIN
+        self.pub_task_finished.publish(msg)
+        return
+
+    def wait_for_task_command(self):
+        while not self.start_task:
+            pass
+        time.sleep(2)
 
 
     def save_result(self):
@@ -68,11 +96,11 @@ class CameraCalibration():
         np.save(self.result_path + "/R_cam2gripper.npy", self.R_cam2gripper)
         np.save(self.result_path + "/t_cam2gripper.npy", self.t_cam2gripper)
 
-        #Save homogenous Matrix Cam2Gripper
-        temp = np.array([0, 0, 0, 1])
-        HM_cam2gripper = np.c_[self.R_cam2gripper, self.t_cam2gripper]
-        HM_cam2gripper = np.r_[HM_cam2gripper, np.reshape(temp, (1, 4))]
+        HM_cam2gripper = self.rmat_tvec_to_hm(self.R_cam2gripper, self.t_cam2gripper)
+        HM_gripper2cam = inv(HM_cam2gripper)
         np.save(self.result_path + "/HM_cam2gripper.npy", HM_cam2gripper)
+        np.save(self.result_path + "/HM_gripper2cam.npy", HM_gripper2cam)
+        
     
 
 def main(args):       
